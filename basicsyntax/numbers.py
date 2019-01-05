2d20bef9de965127608950edd47e36d6af8acf6e
bdebelle <brian.debelle@gmail.com>
2019-01-05 14:20:18 -0800

    lesson 3_4

---
+++ basicsyntax/numbers.py
@@ -0,0 +1,24 @@
+int_num = 10
+float_num = 20.0
+
+print(int_num)
+print(float_num)
+
+a = 10
+b = 20
+
+add = a + b
+
+print(add)
+
+sub = b - a
+
+multi = a * b
+
+print(sub)
+print(multi)
+
+div = a / b
+
+print(div)
+
