bc25a786f7af6fe9a40e4434c84fa7eaa5f00660
bdebelle <brian.debelle@gmail.com>
2019-01-05 17:13:23 -0800

    lesson 3.5

---
+++ basicsyntax/numbers_operations.py
@@ -0,0 +1,17 @@
+# This is a one line comment
+
+# Exponentiation
+
+exponents = 10 ** 20
+print(exponents)
+
+"""
+this is a multi line comment
+Modulo -- it returns the remainder
+"""
+remainder = 11 % 3
+print(remainder)
+
+modulo = 37 % 4
+
+print(modulo)
