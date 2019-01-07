72f1cc6a99e5a2f134f51391e1b73bd8c8ac5207
bdebelle <brian.debelle@gmail.com>
2019-01-07 13:11:10 -0800

    lesson 3.8 strings

---
+++ basicsyntax/strings.py
@@ -0,0 +1,26 @@
+"""
+examples of string and how they work in python
+Sequence of characters
+contains a-z, 0-9, @
+MUST be in double or single quotes
+"""
+
+a = "this is a simple string"
+b = 'using single quotes'
+print(a)
+print(b)
+
+c= "Need to use 'quotes' inside a string"
+d = 'These "quotes" also work in reverse'
+
+print(c)
+print(d)
+
+e = "Another way to handle \"quotes\" is to use the backslash"
+
+print(e)
+
+f = "this is a single string \
+that we wrote on 2 lines"
+
+print(f)
