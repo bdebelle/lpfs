197d521de2ac6cb6a5109c703b36bc0407a7eff0
bdebelle <brian.debelle@gmail.com>
2019-01-07 21:30:51 -0800

    lesson 3.12

---
+++ basicsyntax/string_formatting.py
@@ -0,0 +1,9 @@
+"""
+Examples of how string formatting works in python
+"""
+city="nyc"
+event="show"
+
+print("Welcome to " + city +" and enjoy the " + event)
+print("Welcome to %s" %(city))
+print("Welcome to %s and enjoy the %s" %(city, event))
