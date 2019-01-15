60d4f526a7288e6eda1bd8833f6f0e098e564484
bdebelle <brian.debelle@gmail.com>
2019-01-14 18:33:46 -0800

    Lesson 5.1

---
+++ controlstructure/comparators.py
@@ -0,0 +1,33 @@
+"""
+The Truth Terms -- we have the following terms
+to determine if something is True or False
+
+== --> value equality
+!= --> Not equal to
+< --> Less than
+> --> greater than
+<= --> less than or equal to
+>= --> Greater than or equal to
+"""
+
+bool_one = 10 == 10
+print(bool_one)
+
+bool_two = 11 == 10
+print(bool_two)
+
+not_equal = 10 != 10
+print(not_equal)
+
+less_than = 10 < 6
+print(less_than)
+
+greater_than = 10 > 1
+print(greater_than)
+
+lt_eq = 10 <= 10
+print(lt_eq)
+
+gt_eq = 10 >= 7
+print(gt_eq)
+
