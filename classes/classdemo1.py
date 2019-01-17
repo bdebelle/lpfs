92918cb0df2c5f78a370c5e778493079fee97edb
bdebelle <brian.debelle@gmail.com>
2019-01-16 20:35:53 -0800

    lesson 8.1

--- classes/classdemo1.py
+++ classes/classdemo1.py
@@ -1,3 +1,15 @@
 """
 Object oriented programming
 """
+
+#Define an object of the string class
+s = 'this is a string'
+a = 'another string'
+#upper and lower are methods working on 's' the object
+s.upper()
+s.lower()
+
+#'s' is and object of the string class
+print(type('s'))
+print(type('a'))
+print(type([1,2,3]))
