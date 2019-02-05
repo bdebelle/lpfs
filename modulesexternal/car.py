c3c3ccc9f153fe615f0e7c1a223078386aa68640
bdebelle <brian.debelle@gmail.com>
2019-02-04 20:08:07 -0800

    Lesson 10.2

---
+++ modulesexternal/car.py
@@ -0,0 +1,8 @@
+"""
+this is our module which dose not exist in the
+python builtins
+"""
+
+def info(make, model):
+    print("Make of the car: "+ make)
+    print("Model of the car: "+ model)
--- modulespackage/modulesdemo.py
+++ modulespackage/modulesdemo.py
@@ -5,12 +5,21 @@ https://docs.python.org/3/library
 import math
 #imports only sqrt - this saves on memory
 from math import sqrt
-
+# import the car file from the other package
+# import modulesexternal.car as car
+from modulesexternal.car import info
 class ModulesDemo():
 
     def builtin_modules(self):
         print(math.sqrt(100))
         print(sqrt(1000))
 
+    def car_description(self):
+        make = 'bmw'
+        model = '550i'
+        #info is imported from car.py
+        info(make, model)
+
 m = ModulesDemo()
-m.builtin_modules()
+#m.builtin_modules()
+m.car_description()
