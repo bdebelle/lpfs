c5d8f0ff79331378773bf04f9ed1b1b2dd7453d3
bdebelle <brian.debelle@gmail.com>
2019-02-05 19:53:44 -0800

    Lesson 12.3 console logger

---
+++ loggingpackage/logger_demo_console.py
@@ -0,0 +1,36 @@
+"""
+Logger Demo
+"""
+import logging
+
+class LoggerDemoConsole():
+
+    def testLog(self):
+        #create logger
+        logger = logging.getLogger(LoggerDemoConsole.__name__)
+        logger.setLevel(logging.INFO)
+
+        # Create console handler and set level to info
+        chandler = logging.StreamHandler()
+        chandler.setLevel(logging.INFO)
+
+        # Create Formatter
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p')
+
+        # add formatter to console handler -> ch
+        chandler.setFormatter(formatter)
+
+        #add console handler to logger
+        logger.addHandler(chandler)
+
+
+        #logging messages
+        logger.debug('debug message')
+        logger.info('info message')
+        logger.warning('warn message')
+        logger.error('error message')
+        logger.critical('critical message')
+
+demo = LoggerDemoConsole()
+demo.testLog()
